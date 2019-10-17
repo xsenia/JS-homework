94f8430f73ad050270fa5b644d17e28158d67179
xsenia <belkinasan@yandex.ru>
2019-10-17 20:59:45 +0500

    first commit

---
+++ lesson-01/script.js
@@ -0,0 +1,13 @@
+let money = 1000,                          
+    income,                        
+    addExpenses, 
+    deposit,                          
+    mission,                        
+    period;
+
+
+alert('Привет, мир!');
+
+console.log(typeof money);
+
+
