03ce2e6d39af703650b7cc74b1f6fe76cc5201e1
xsenia <belkinasan@yandex.ru>
2019-11-03 11:07:43 +0500

    8 урок

--- lesson-08/script/script.js
+++ lesson-08/script/script.js
@@ -15,7 +15,7 @@ let btnStart =                  document.getElementById('start'),
     additionalIncomeItem =      document.querySelectorAll('.additional_income-item'),
     expensesTitle =             document.querySelector('.expenses-title'),
     expensesAmount =            document.querySelector('.expenses-amount'),
-    additionalExpensesItem =   document.querySelector('.additional_expenses-item'),
+    additionalExpensesItem =    document.querySelector('.additional_expenses-item'),
     depositCheck =              document.querySelector('#deposit-check'),
     depositAmount =             document.querySelector('.deposit-amount'),
     depositPercent =            document.querySelector('.deposit-percent'),
@@ -36,7 +36,9 @@ let btnStart =                  document.getElementById('start'),
     salaryAmount.addEventListener('input', function(){
         if(salaryAmount.value !== '' && !isNaN(salaryAmount.value)) {
             btnStart.removeAttribute('disabled');
-        }
+        } else if(salaryAmount.value === '') {
+            btnStart.setAttribute('disabled', true);
+        }        
     });
    
     let appData = {
