9d4a7bc2c3b965fd558f8f22ada228737529b6b6
xsenia <belkinasan@yandex.ru>
2019-11-18 23:28:00 +0500

    практика 2

--- lesson-14-practice/script/script.js
+++ lesson-14-practice/script/script.js
@@ -140,7 +140,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //scrollTo
 
-    const scrollToAnimate = () => {
+    const scrollToAnimate = () => { 
 
         let linkNav = document.querySelectorAll('menu ul li a'),
         V = 0.7  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
@@ -150,7 +150,7 @@ window.addEventListener('DOMContentLoaded', function() {
                 e.preventDefault();
 
                 let scrollWindow = window.pageYOffset, //текущая прокрутка
-                    hash = this.href.replace(/[^#]*(.*)/, '$1'),  // к id элемента, к которому нужно перейти
+                    hash = this.href.replace(/[^#]*(.*)/, '$1'),  // к id элемента, к которому нужно перейти   
                     top = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
                     start = null;
                 
