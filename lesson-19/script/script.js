343f492c2d1deaca76940bea82a320cc8c26a78d
xsenia <belkinasan@yandex.ru>
2019-11-26 23:10:42 +0500

    19 lesson FETCH

--- lesson-19/script/script.js
+++ lesson-19/script/script.js
@@ -486,7 +486,10 @@ window.addEventListener('DOMContentLoaded', function() {
             
             
             postData(body)
-            .then(() => {
+            .then((response) => {
+                    if(response.status !== 200) {
+                        throw new Error('статус сети не 200');
+                    }
                     statusMessage.textContent = successMessage;
                 })
             .catch((error) => {                
@@ -501,27 +504,13 @@ window.addEventListener('DOMContentLoaded', function() {
 
         const postData = (body) => {
 
-            return new Promise ((resolve, reject) => {
-                const request = new XMLHttpRequest();
-
-                request.addEventListener('readystatechange', () => {  
-                    if(request.readyState !== 4) {
-                        return;                }
-        
-                    if(request.status === 200) {
-                        resolve();                    
-                    } else {
-                        reject(request.status);                    
-                    }
-                });
-
-                request.open('POST', './server1.php');
-                request.setRequestHeader('Content-Type', 'application/json');            
-
-                request.send(JSON.stringify(body));
+            return fetch('./server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
             });
-
-            
         };
 
         const resetForm = (formId) => {
